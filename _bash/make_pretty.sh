7eb0785e074cffbd2609c76bd02c193ebef5bccb
mpaulweeks <mpaulweeks@gmail.com>
2017-11-14 23:12:31 -0500

    prettyify

--- _bash/make_pretty.sh
+++ _bash/make_pretty.sh
@@ -1,3 +1,11 @@
+prettify() {
+  python -c "import sys,json;print json.dumps(json.load(sys.stdin),indent=2,separators=(',', ': '))"
+}
+
 cp _data/bookmarks.json temp.json
-python -m json.tool temp.json > _data/bookmarks.json
+cat temp.json | prettify > _data/bookmarks.json
+rm temp.json
+
+cp _data/conventions.json temp.json
+cat temp.json | prettify > _data/conventions.json
 rm temp.json
