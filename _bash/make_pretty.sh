787c029b1e5e8146a148f63a8317724a4ad4d37f
mpaulweeks <mpaulweeks@gmail.com>
2018-03-24 18:40:49 -0400

    improve make_pretty

--- _bash/make_pretty.sh
+++ _bash/make_pretty.sh
@@ -1,11 +1,11 @@
-prettify() {
+prettify_temp() {
   python -c "import sys,json;print json.dumps(json.load(sys.stdin),indent=2,separators=(',', ': '),sort_keys=True)"
 }
 
-cp _data/bookmarks.json temp.json
-cat temp.json | prettify > _data/bookmarks.json
-rm temp.json
+prettify() {
+  cp $1 temp.json
+  cat temp.json | prettify_temp > $1
+  rm temp.json
+}
 
-cp _data/conventions.json temp.json
-cat temp.json | prettify > _data/conventions.json
-rm temp.json
+for f in _data/*.json; do prettify "$f"; done
